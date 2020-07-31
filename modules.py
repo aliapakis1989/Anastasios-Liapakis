ee31b149cb5ba3203db996c0a72fa9dc81e90b77
aliapakis1989 <67995404+aliapakis1989@users.noreply.github.com>
2020-07-31 11:56:37 +0300

    Update modules.py

--- modules.py
+++ modules.py
@@ -10,7 +10,7 @@ def allagi(lex,apo,se):
         lex=lex.replace(apo[i],se[i])
     return lex
 def read_replace():
-    arxeio=open("C:\PhD/aaaa.txt",'r',encoding="utf8")
+    arxeio=open("C:/modules/aaaa.txt",'r',encoding="utf8")
     allagi_apo=[]
     allagi_se=[]
     for rec in arxeio:
